705d0bf7d3f938efd806d70f6a3cecb5a737ebb7
Monika Yadav <monikay0019@gmail.com>
2026-09-20 10:26:42 +0530

    Time: 0 ms (100%), Space: 9.1 MB (31.63%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Take one digit
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // Take two digits
+            if (i >= 2) {
+                int num = stoi(s.substr(i - 2, 2));
+
+                if (num >= 10 && num <= 26) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
